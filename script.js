e32c457a73946598fafe07a6eaf3ae0196d447c2
Dung Hoang <hvdung1791@gmail.com>
2026-06-28 21:54:40 +0700

    ECTM AI V3 LocalStorage

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("ectm_tasks")) || [];
 
 function addTask() {
-
     const input = document.getElementById("taskInput");
     const priority = document.getElementById("priority");
 
@@ -12,29 +11,48 @@ function addTask() {
 
     tasks.push({
         name: input.value,
-        priority: priority.value
+        priority: priority.value,
+        done: false
     });
 
     input.value = "";
-
+    saveTasks();
     renderTasks();
 }
 
+function toggleTask(index) {
+    tasks[index].done = !tasks[index].done;
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+function saveTasks() {
+    localStorage.setItem("ectm_tasks", JSON.stringify(tasks));
+}
+
 function renderTasks() {
-
     const list = document.getElementById("taskList");
-
     list.innerHTML = "";
 
-    tasks.forEach((task) => {
-
+    tasks.forEach((task, index) => {
         const li = document.createElement("li");
 
-        li.innerHTML =
-            "<b>" + task.priority + "</b> - " + task.name;
+        li.innerHTML = `
+            <span style="${task.done ? 'text-decoration: line-through; color: gray;' : ''}">
+                <b>${task.priority}</b> - ${task.name}
+            </span>
+            <button onclick="toggleTask(${index})">Xong</button>
+            <button onclick="deleteTask(${index})">Xóa</button>
+        `;
 
         list.appendChild(li);
-
     });
-
 }
+
+renderTasks();
